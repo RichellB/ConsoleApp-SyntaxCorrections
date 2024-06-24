891ec7e28d46b3f75e8a6e997ec7c832e7b10f26
RichellB <54103270+RichellB@users.noreply.github.com>
2024-06-24 00:28:16 -0400

    Added PersonalInfo program

---
+++ PersonalInfo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    // Declaring variables
+    std::string firstName = "Richell";
+    std::string lastName = "Blackwood";
+    std::string streetAddress = "123 Main Street";
+    std::string city = "Philadelphia";
+    std::string zipCode = "12345";
+
+    // Printing Personal Info
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
